94085c864c03ece3151fbc1dabcfbf4c3b67c828
atreece20@gmail.com <atreece20@gmail.com>
2022-05-22 21:09:00 -0700

    beginning implementation of routing to new pages

--- goodgamersapp/src/App.js
+++ goodgamersapp/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.PNG';
 import './App.css';
 import {Genres, Companies, Decades, ArtStyles, Platforms, PriceRange} from './DropDowns.js'
+import {SearchResults} from './Searchresults.js';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
 
@@ -13,29 +15,44 @@ function App() {
           Welcome to the Good Gamers database!
         </p>
         <p>
-          Our goal is to provide you with the best selection of video titles.
-          Please search for your favorite video game preferences
-        </p>
-        <input type="text" />
-        <p>
-          Additionally, you can use our drop down menus to search specific categories
+          Our goal is to provide you with the best selection of video titles!
         </p>
         {/* <script type="text/javascript" src="./Genres.js"></script> */}
-        <Genres/>
-        <Companies/>
-        <Decades/>
-        <ArtStyles/>
-        <Platforms/>
-        <PriceRange/>
-        {/* <a
+        
+        
+      </header>
+
+      <body className="App-body">
+      Please search for your favorite video game title
+      </body>
+      <body className="App-search">
+      <input type="text" />
+      <Router>
+        <button>
+          <Link to="/Searchresults">search</Link>
+        </button> 
+        <Routes>
+          <Route exact path = "/Searchresults" element = {<SearchResults/>}> </Route>
+        </Routes>  
+      </Router>
+      
+      {/* /* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a> */}
-      </header>
+        </a> */ }
+      
+      </body>
+
+      <body className="App-buttons">
+        <p>
+          Additionally, you can use our drop down menus to search specific categories
+        </p>
+      <Genres/> <Companies/> <Decades/> <ArtStyles/> <Platforms/><PriceRange/>
+      </body>
     </div>
   );
 }
---
+++ goodgamersapp/src/Searchresults.js
@@ -0,0 +1,13 @@
+//import React, {Component} from 'react';
+import logo from './logo.PNG';
+import './Searchresults.css';
+
+function SearchResults () {
+    return (
+      <div className="searchResults">
+          Search results appearing now
+      </div>
+    );
+}
+
+export {SearchResults};
